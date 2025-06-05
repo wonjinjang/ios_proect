429e3683e78ce8675f187aca445fe92dc4bc9814
장원진 <jjwj0228@naver.com>
2025-06-05 20:57:23 +0900

    첫 커밋

---
+++ CityGuideApp/CityGuideApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CityGuideApp
+//
+//  Created by 장원진 on 6/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CityGuideApp/CityGuideAppTests/CityGuideAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CityGuideAppTests.swift
+//  CityGuideAppTests
+//
+//  Created by 장원진 on 6/5/25.
+//
+
+import Testing
+@testable import CityGuideApp
+
+struct CityGuideAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
